cfb5c1dcedc329814e28feb9727403e7d50faa1c
praczkowski <p.raczkowski@ocado.com>
2021-02-05 23:01:54 +0100

    server.js added

--- src/main/java/com/example/customers/CustomerController.java
+++ src/main/java/com/example/customers/CustomerController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "https://customers-frontened.herokuapp.com")
 public class CustomerController {
 
     CustomerService customerService;
